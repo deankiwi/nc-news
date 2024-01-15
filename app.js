268453ac5013092abfc9f9320a5d727e64bf2d3e
Dean <dean.w1993@gmail.com>
2024-01-15 21:35:26 +0000

    Implement function for endpoint /api

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
+const { getEndPoints } = require("./controllers/api.controllers");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", getEndPoints);
 
 app.use((err, req, res, next) => {
   console.log(err);
