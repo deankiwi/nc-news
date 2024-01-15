a95d423bc95d851b289198f8c87c755ae2e38489
Dean <dean.w1993@gmail.com>
2024-01-15 14:45:06 +0000

    Add test for endpoint /api/topics

---
+++ __tests__/topics.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+
+afterAll(() => db.end());
+
+beforeEach(() => seed(data));
+
+describe("/api/topics", () => {
+  test("should return array of topic objects each with 'slug' and 'description' key values", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body;
+        expect(Array.isArray(topics)).toBe(true);
+        expect(topics.length).toBeGreaterThan(0);
+        topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+        });
+      });
+  });
+});
