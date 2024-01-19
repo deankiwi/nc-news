535c84cc57b0c5f2c9098f3abf7f113ba01494b8
Dean <dean.w1993@gmail.com>
2024-01-19 10:36:55 +0000

    feat : enhance GET /api/articles/:article_id to include comment_count in response
    - Add tests for the updated functionality
    - Update implementation to return an article object instead of an array

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,8 +3,25 @@ const db = require("../db/connection");
 exports.fetchArticleById = (article_id) => {
   return db
     .query(
-      `SELECT * FROM articles
-  WHERE article_id = $1`,
+      `
+  SELECT 
+      articles.author,
+      articles.title,
+      articles.article_id,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+      articles.body,
+      COUNT(comments.article_id) AS comment_count
+  FROM 
+      articles
+  LEFT JOIN 
+      comments ON articles.article_id = comments.article_id
+  WHERE articles.article_id = $1
+  GROUP BY
+      articles.article_id
+  ORDER BY articles.created_at DESC;`,
       [article_id]
     )
     .then(({ rows }) => {
@@ -14,7 +31,7 @@ exports.fetchArticleById = (article_id) => {
           msg: "article_id not found",
         });
       }
-      return rows;
+      return rows[0];
     });
 };
 
